50fdc1737925a93aa3b06b493c56e4407661ec92
GuiSipoloni <guilhermesipoloni@hotmail.com>
2018-05-10 11:40:16 -0300

    return only 201 create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ module Api
       def create
         user = User.new(user_params)
         if user.save
-          render json: { user: user }, status: :created
+          render json: {}, status: :created
         end
         render json: user.errors, status: :bad_request
       end
