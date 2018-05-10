05a61e1f8d9f61c9696199a2f69b69414efa6e2b
GuiSipoloni <guilhermesipoloni@hotmail.com>
2018-05-10 11:51:52 -0300

    return when create sucessful

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,7 @@ module Api
       def create
         user = User.new(user_params)
         if user.save
-          render json: {}, status: :created
+          return render json: {}, status: :created
         end
         render json: user.errors, status: :bad_request
       end
